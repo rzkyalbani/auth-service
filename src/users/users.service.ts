62b1bf104b60da58760c3d5f79c6bf386c5582a5
rzkyalbani <rzkyalbani@gmail.com>
2025-10-29 07:44:20 +0700

    feat: setup PrismaModule and UsersModule with create/find services

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
