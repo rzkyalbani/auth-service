3f24328638dafa81d6f338635017b1b3ca368674
Rizky Albani <rzkyalbani@gmail.com>
2025-11-08 14:09:49 +0700

    feat(cors): enable CORS with specific origin and methods for enhanced API access

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
 
   app.use(helmet());
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
